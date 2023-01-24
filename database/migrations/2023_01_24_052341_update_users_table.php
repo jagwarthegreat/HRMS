c54f3eacf45ff4f162d20097de5a31d4f39d6388
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2023-01-24 16:55:26 +0800

    updated

--- database/migrations/2023_01_24_052341_update_users_table.php
+++ database/migrations/2023_01_24_052341_update_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('limited_access_status', 1)->default(0);
+            $table->integer('limited_access_status')->default(0);
         });
     }
 
